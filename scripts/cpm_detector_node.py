c3a3f446c785a2698f6e8f7155b96f893cfc4ce0
Vivek CHuahan <viveksdchauhan@gmail.com>
2018-06-17 23:38:22 +0100

    no message

---
+++ scripts/cpm_detector_node.py
@@ -0,0 +1,209 @@
+#!/usr/bin/env python
+# This file is responsible for bridging ROS to the ObjectDetector class (built with PyCaffe)
+
+from __future__ import division
+
+import sys
+import rospy
+import pcl
+import numpy as np
+import ctypes
+import struct
+import sensor_msgs.point_cloud2 as pc2
+
+from sensor_msgs.msg import PointCloud2, PointField
+from std_msgs.msg import Header
+from random import randint
+import cv2
+import rospy
+import numpy as np
+from cv_bridge import CvBridge, CvBridgeError
+from sensor_msgs.msg import Image, CompressedImage
+from rail_pose_estimator.msg import Keypoints, Poses
+
+import pose_estimation
+
+# Debug Helpers
+FAIL_COLOR = '\033[91m'
+ENDC_COLOR = '\033[0m'
+
+
+def eprint(error):
+    sys.stderr.write(
+        FAIL_COLOR
+        + type(error).__name__
+        + ": "
+        + error.message
+        + ENDC_COLOR
+    )
+# End Debug Helpers
+
+
+class CPM(object):
+    """
+    This class takes in image data and finds / annotates objects within the image
+    """
+
+    def __init__(self):
+        rospy.init_node('rail_pose_estimator_node')
+        self.person_keypoints = []
+        self.keypoint_arrays = []
+        self.image_datastream = None
+        self.input_image = None
+        self.bridge = CvBridge()
+        self.detector = pose_estimation.PoseMachine()
+        self.debug = rospy.get_param('~debug', default=False)
+        self.image_sub_topic_name = rospy.get_param('~image_sub_topic_name', default='/camera/rgb/image_raw')
+        self.use_compressed_image = rospy.get_param('~use_compressed_image', default=False)
+        self.part_str = ['nose', 'neck', 'right_shoulder', 'right_elbow', 'right_wrist', 'left_shoulder', 'left_elbow',
+                      'left_wrist', 'Rhip', 'Rkne', 'Rank', 'Lhip', 'Lkne', 'Lank', 'right_eye', 'left_eye', 'right_ear',
+                      'left_ear']
+
+    def _convert_msg_to_image(self, image_msg):
+        """
+        Convert an incoming image message (compressed or otherwise) into a cv2
+        image
+        """
+        if not self.use_compressed_image:
+            try:
+                image_cv = self.bridge.imgmsg_to_cv2(image_msg, "bgr8")
+            except CvBridgeError as e:
+                print e
+                return None
+        else:
+            image_np = np.fromstring(image_msg.data, np.uint8)
+            image_cv = cv2.imdecode(image_np, cv2.CV_LOAD_IMAGE_COLOR)
+
+        return image_cv
+
+    def _parse_image(self, image_msg):
+
+        header = image_msg.header
+        image_cv = self._convert_msg_to_image(image_msg)
+        if image_cv is None:
+            return
+        # self.person_keypoints = self.detector.estimate_keypoints(image_cv)
+        # candidate, subset = self.detector.estimate_keypoints(image_cv)
+        people = self.detector.estimate_keypoints(image_cv)
+        #### DEBUG ####
+        if self.debug:
+            # out_image = self.detector.visualize_keypoints(image_cv, candidate, subset)
+            out_image = self.detector.visualize_keypoints(image_cv, people)
+            try:
+                image_msg = self.bridge.cv2_to_imgmsg(out_image, "bgr8")
+            except CvBridgeError as e:
+                print e
+
+            image_msg.header = header
+            self.image_pub.publish(image_msg)
+        #### END DEBUG ####
+
+        # Instantiate poses object
+        obj_arr = Poses()
+        obj_arr.header = header
+        for person in people:
+            msg = Keypoints()
+            nose = person.get('nose', [-1, -1])
+            msg.nose_y = nose[0]
+            msg.nose_x = nose[1]
+
+            neck = person.get('neck', [-1, -1])
+            msg.neck_y = neck[0]
+            msg.neck_x = neck[1]
+
+            right_shoulder = person.get('right_shoulder', [-1, -1])
+            msg.right_shoulder_y = right_shoulder[0]
+            msg.right_shoulder_x = right_shoulder[1]
+            left_shoulder = person.get('left_shoulder', [-1, -1])
+            msg.left_shoulder_y = left_shoulder[0]
+            msg.left_shoulder_x = left_shoulder[1]
+
+            right_elbow = person.get('right_elbow', [-1, -1])
+            msg.right_elbow_y = right_elbow[0]
+            msg.right_elbow_x = right_elbow[1]
+            left_elbow = person.get('left_elbow', [-1, -1])
+            msg.left_elbow_y = left_elbow[0]
+            msg.left_elbow_x = left_elbow[1]
+
+            right_wrist = person.get('right_wrist', [-1, -1])
+            msg.right_wrist_y = right_wrist[0]
+            msg.right_wrist_x = right_wrist[1]
+            left_wrist = person.get('left_wrist', [-1, -1])
+            msg.left_wrist_y = left_wrist[0]
+            msg.left_wrist_x = left_wrist[1]
+
+            Lhip = person.get('Lhip', [-1, -1])
+            msg.left_hip_y = Lhip[0]
+            msg.left_hip_x = Lhip[1]
+            Rhip = person.get('Rhip', [-1, -1])
+            msg.right_hip_y = Rhip[0]
+            msg.right_hip_x = Rhip[1]
+
+            left_eye = person.get('left_eye', [-1, -1])
+            msg.left_eye_y = left_eye[0]
+            msg.left_eye_x = left_eye[1]
+            right_eye = person.get('right_eye', [-1, -1])
+            msg.right_eye_y = right_eye[0]
+            msg.right_eye_x = right_eye[1]
+
+            right_ear = person.get('right_ear', [-1, -1])
+            msg.right_ear_y = right_ear[0]
+            msg.right_ear_x = right_ear[1]
+            left_ear = person.get('left_ear', [-1, -1])
+            msg.left_ear_y = left_ear[0]
+            msg.left_ear_x = left_ear[1]
+
+            Rkne = person.get('Rkne', [-1, -1])
+            msg.right_knee_y = Rkne[0]
+            msg.right_knee_x = Rkne[1]
+            Lkne = person.get('Lkne', [-1, -1])
+            msg.left_knee_y = Lkne[0]
+            msg.left_knee_x = Lkne[1]
+
+            Rank = person.get('Rank', [-1, -1])
+            msg.right_ankle_y = Rank[0]
+            msg.right_ankle_x = Rank[1]
+            Lank = person.get('Lank', [-1, -1])
+            msg.left_ankle_y = Lank[0]
+            msg.left_ankle_x = Lank[1]
+
+            obj_arr.people.append(msg)
+
+        self.object_pub.publish(obj_arr)
+
+    def read_points(self, points):
+        # print points
+        pass
+
+    def pcl_callback(self, pcl_msg):
+        all_points = np.array([3])
+        print all_points
+        for point in pc2.read_points(pcl_msg, skip_nans=True):
+            pt_x = point[0]
+            pt_y = point[1]
+            pt_z = point[2]
+
+            points = np.array([pt_x, pt_y, pt_z])
+            all_points = np.append(all_points, points)
+            self.read_points(all_points)
+
+
+    def run(self,
+            pub_image_topic='~debug/poses_image',
+            pub_object_topic='~poses'):
+        if not self.use_compressed_image:
+            rospy.Subscriber(self.image_sub_topic_name, Image, self._parse_image)
+            rospy.Subscriber("/pcl_table", pc2.PointCloud2, self.pcl_callback, queue_size=1)
+        else:
+            rospy.Subscriber(self.image_sub_topic_name+'/compressed', CompressedImage, self._parse_image)
+        if self.debug:
+            self.image_pub = rospy.Publisher(pub_image_topic, Image, queue_size=2) # image publisher
+        self.object_pub = rospy.Publisher(pub_object_topic, Poses, queue_size=2) # objects publisher
+        rospy.spin()
+
+if __name__ == '__main__':
+    try:
+        detector = CPM()
+        detector.run()
+    except rospy.ROSInterruptException:
+        pass
---
+++ scripts/seg.py
@@ -0,0 +1,99 @@
+from pcl_helper import *
+
+def pcl_callback(pcl_msg):
+    # vox = ros_to_pcl(pcl_msg)
+    # Convert ROS msg to PCL data
+    cloud = ros_to_pcl(pcl_msg)
+
+    # Voxel Grid Downsampling
+    vox = cloud.make_voxel_grid_filter()
+    LEAF_SIZE = 0.01
+    vox.set_leaf_size(LEAF_SIZE, LEAF_SIZE, LEAF_SIZE)
+    cloud_filtered = vox.filter()
+
+    # PassThrough Filter (isolate the table and objects from the rest)
+    passthrough = cloud_filtered.make_passthrough_filter()
+    filter_axis = 'z'
+    passthrough.set_filter_field_name(filter_axis)
+    axis_min = 0.77  # all under axis_min [m] is erased
+    axis_max = 1.1  # all over axis_max [m] is erased
+    passthrough.set_filter_limits(axis_min, axis_max)
+    cloud_filtered = passthrough.filter()
+
+    # RANSAC Plane Segmentation to identiyfy the table
+    seg = cloud_filtered.make_segmenter()
+    seg.set_model_type(pcl.SACMODEL_PLANE)
+    seg.set_method_type(pcl.SAC_RANSAC)
+    # how close a point must be to the model in order to be considered as an inlier
+    max_distance = 0.005  # [m]
+    seg.set_distance_threshold(max_distance)
+
+    # Extract inliers and outliers
+    inliers, coefficients = seg.segment()
+    cloud_table = cloud_filtered.extract(inliers, negative=False)
+    cloud_objects = cloud_filtered.extract(inliers, negative=True)
+
+    # Euclidean Clustering
+    # Construct k-d tree (cloud with only spatial (raeumlich) information, colorless cloud)
+    white_cloud = XYZRGB_to_XYZ(cloud_objects)
+    tree = white_cloud.make_kdtree()
+    # Create a cluster extraction object
+    ec = white_cloud.make_EuclideanClusterExtraction()
+    # Set tolerances for distance threshold
+    # as well as minimum and maximum cluster size (in points)
+    # NOTE: These are poor choices of clustering parameters
+    # Your task is to experiment and find values that work for segmenting objects.
+    ec.set_ClusterTolerance(0.04)  # [m]
+    ec.set_MinClusterSize(150)
+    ec.set_MaxClusterSize(1500)
+    # Search the k-d tree for clusters
+    ec.set_SearchMethod(tree)
+
+    # Extract indices for each of the discovered clusters
+    cluster_indices = ec.Extract()
+
+    # Create Cluster-Mask Point Cloud to visualize each cluster separately
+    # Assign a color corresponding to each segmented object in scene
+    cluster_color = get_color_list(len(cluster_indices))
+
+    color_cluster_point_list = []
+
+    for j, indices in enumerate(cluster_indices):
+        for i, indice in enumerate(indices):
+            color_cluster_point_list.append([white_cloud[indice][0], white_cloud[indice][1], white_cloud[indice][2],
+                                             rgb_to_float(cluster_color[j])])
+
+    # Create new cloud containing all clusters, each with unique color
+    cluster_cloud = pcl.PointCloud_PointXYZRGB()
+    cluster_cloud.from_list(color_cluster_point_list)
+    #
+    # Convert PCL data to ROS messages
+    ros_cloud_objects = pcl_to_ros(cloud_objects)
+    ros_cloud_table = pcl_to_ros(cloud_table)
+    ros_cluster_cloud = pcl_to_ros(cluster_cloud)
+
+    # Publish ROS messages
+    pcl_objects_pub.publish(ros_cloud_objects)
+    pcl_table_pub.publish(ros_cloud_table)
+    pcl_cluster_pub.publish(ros_cluster_cloud)
+
+
+if __name__ == '__main__':
+
+    # ROS node initialization
+    rospy.init_node('clustering', anonymous=True)
+
+    # Create Subscribers
+    pcl_sub = rospy.Subscriber("/camera/depth_registered/points", pc2.PointCloud2, pcl_callback, queue_size=1)
+
+    # Create Publishers
+    pcl_objects_pub = rospy.Publisher("/pcl_objects", pc2.PointCloud2, queue_size=1)
+    pcl_table_pub = rospy.Publisher("/pcl_table", pc2.PointCloud2, queue_size=1)
+    pcl_cluster_pub = rospy.Publisher("/pcl_cluster", pc2.PointCloud2, queue_size=1)
+
+    # Initialize color_list
+    # get_color_list.color_list = []
+
+    # Spin while node is not shutdown
+    while not rospy.is_shutdown():
+ 	rospy.spin()
